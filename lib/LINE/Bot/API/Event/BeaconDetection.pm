c06db5f9ef0906192bfae755d04a71bfb392d802
Kazuhiro Osawa <yappo@shibuya.pl>
2016-09-23 23:30:04 +0900

    no message

--- lib/LINE/Bot/API/Event/BeaconDetection.pm
+++ lib/LINE/Bot/API/Event/BeaconDetection.pm
@@ -5,7 +5,7 @@ use parent 'LINE::Bot::API::Event::Base';
 
 sub is_beacon_detection_event { 1 }
 
-sub beacon_hwid { $_[0]->{message}{'beacon.hwid'} }
-sub beacon_type { $_[0]->{message}{'beacon.type'} }
+sub beacon_hwid { $_[0]->{'beacon.hwid'} }
+sub beacon_type { $_[0]->{'beacon.type'} }
 
 1;
