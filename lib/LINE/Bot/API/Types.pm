cc98a9b86813eb840db17c85eb398f51869c6fcf
Kang-min Liu <gugod@gugod.org>
2019-03-22 15:52:16 +0900

    define more types for "MessageEvent"

--- lib/LINE/Bot/API/Types.pm
+++ lib/LINE/Bot/API/Types.pm
@@ -4,11 +4,51 @@ use warnings;
 
 use Type::Library -base;
 use Type::Utils -all;
-use Types::Standard qw<Str Int Bool Dict Optional ArrayRef HashRef>;
+use Types::Standard qw<Str Int Bool Dict Enum Optional ArrayRef HashRef>;
 use Types::Common::Numeric qw< PositiveOrZeroInt >;
+use Types::Common::String qw< StrLength NonEmptyStr >;
 
-declare TextMessage => as Dict[ type => Str, text => Str ], where {
-    $_->{type} eq 'text' && length( $_->{text} ) <= 2000
-};
+my $ReplyToken = declare ReplyToken => as Str;
+
+# https://developers.line.biz/en/reference/messaging-api/#source-user
+declare SourceUser => as Dict[
+    type    => Enum["user"],
+    userId  => NonEmptyStr,
+];
+
+# https://developers.line.biz/en/reference/messaging-api/#source-group
+declare SourceGroup => as Dict[
+    type    => Enum["group"],
+    groupId => NonEmptyStr,
+    userId   => Optional[Str],
+];
+
+# https://developers.line.biz/en/reference/messaging-api/#source-room
+declare SourceRoom => as Dict[
+    type    => Enum["room"],
+    groupId => NonEmptyStr,
+    userId  => Optional[NonEmptyStr],
+];
+
+# https://developers.line.biz/en/reference/messaging-api/#wh-text
+my $TextMessage = declare TextMessage => as Dict[ id => Str, type => Enum["text"], text => StrLength[1,2000] ];
+
+# https://developers.line.biz/en/reference/messaging-api/#wh-image
+my $ImageMessage = declare ImageMessage => as Dict[
+    id => Str,
+    type => Enum["image"],
+    contentProvider => Dict[
+        type => Enum["line", "external"],
+        previewImageUrl => Optional[NonEmptyStr],
+        originalContentUrl => Optional[NonEmptyStr],
+    ]
+];
+
+# https://developers.line.biz/en/reference/messaging-api/#message-event
+declare MessageEvent => as Dict[
+    type => Enum["message"],
+    replyToken => NonEmptyStr,
+    message => $TextMessage | $ImageMessage ,
+];
 
 1;
