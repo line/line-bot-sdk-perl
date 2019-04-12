d0409590e126aea8098898864ff56545fa76ce80
Kang-min Liu <gugod@gugod.org>
2019-04-12 16:52:17 +0900

    Define a class for representing "Rich menu response object".
    https://developers.line.biz/en/reference/messaging-api/#rich-menu-response-object

--- lib/LINE/Bot/API/Response/RichMenu.pm
+++ lib/LINE/Bot/API/Response/RichMenu.pm
@@ -4,7 +4,32 @@ use warnings;
 use utf8;
 use parent 'LINE::Bot::API::Response::Common';
 
-sub rich_menu_id     { $_[0]->{richMenuId} }
+
+=head1 NAME
+
+LINE::Bot::API::Response::RichMenu
+
+=head1 DESCRIPTION
+
+This class correspond to the "Rich menu response object" as described in
+this page: L<https://developers.line.biz/en/reference/messaging-api/#rich-menu-response-object>
+
+There is a method for each top-level properties, but values become
+simple perl variablse.  Num, Str, HashRef, or ArrayRef -- instead of
+being objects all the way down.
+
+=cut
+
+sub richMenuId  { $_[0]->{richMenuId} }
+sub chatBarText { $_[0]->{richMenuId} }
+sub size        { $_[0]->{size} }
+sub selected    { $_[0]->{selected} }
+sub name        { $_[0]->{nam} }
+sub areas       { $_[0]->{areas} }
+
+# Aliases
+sub rich_menu_id  { $_[0]->{richMenuId} }
+sub chat_bar_text { $_[0]->{chatBarText} }
 
 1;
 
