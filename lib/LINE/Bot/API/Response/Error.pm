8dd1afe8a5cbb832cc6c112d05ea776eb0726b45
Kang-min Liu <gugod@gugod.org>
2019-06-28 18:10:00 +0900

    Implements both Oauth token APIs (issue + revoke)

--- lib/LINE/Bot/API/Response/Error.pm
+++ lib/LINE/Bot/API/Response/Error.pm
@@ -6,4 +6,8 @@ use parent 'LINE::Bot::API::Response::Common';
 sub message { $_[0]->{message} }
 sub details { $_[0]->{details} }
 
+sub error { $_[0]->{error} }
+sub error_description { $_[0]->{error_description} }
+
+
 1;
---
+++ lib/LINE/Bot/API/Response/Token.pm
@@ -0,0 +1,11 @@
+package LINE::Bot::API::Response::Token;
+use strict;
+use warnings;
+use utf8;
+use parent 'LINE::Bot::API::Response::Common';
+
+sub access_token { $_[0]->{access_token} }
+sub expires_in { $_[0]->{expires_in} }
+sub token_type { $_[0]->{token_type} }
+
+1;
