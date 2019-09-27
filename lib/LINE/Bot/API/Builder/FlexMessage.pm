68690d4e4b15f2fff864668f994d82b0d7c82e9e
Kang-min Liu <gugod@gugod.org>
2019-09-27 17:07:14 +0900

    docuemntation of methods

--- lib/LINE/Bot/API/Builder/FlexMessage.pm
+++ lib/LINE/Bot/API/Builder/FlexMessage.pm
@@ -5,6 +5,7 @@ use warnings;
 
 sub new {
     my ($class, %args) = @_;
+    die "`json` attribute is required" unless defined $args{json};
     return bless { _json => $args{json} }, $class;
 }
 
@@ -27,7 +28,8 @@ __END__
 
 This module can be used to convert the output of L<Flex Message Simulator|https://developers.line.biz/console/fx/> to an object.
 
-Structurally, a flex message is represented as an object in JSON with smaller components. Here's an minimal example:
+Structurally, a flex message is represented as an object in JSON with
+smaller components. Here's an minimal example:
 
     {
       "type": "flex",
@@ -50,6 +52,27 @@ Structurally, a flex message is represented as an object in JSON with smaller co
         }
       }
     }
+
+This module merely parse the given json text provided by the C<json>
+attribute and directly use the structure as the message content.
+
+It is created to let developers able to basically paste the JSON text
+produced by Flex Message Simular into code.
+
+=head1 METHODS
+
+=over 4
+
+=item new( json => $json_text )
+
+Object constructure. The "json" parameter is required. The value
+should be a normal scalar variable containing json text.
+
+=item build()
+
+Returns the message object that can be pass thru push_message method.
+
+=back
 
 =head1 SEE ALSO
 
