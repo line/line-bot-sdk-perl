cd81260bf8a277ae392ba5a1ddf830e7df798533
bedoshi <bedoshidiary.ester.freesia@gmail.com>
2020-04-25 05:06:00 +0900

    add get_data_for_multiple_audience

--- lib/LINE/Bot/Audience.pm
+++ lib/LINE/Bot/Audience.pm
@@ -4,12 +4,14 @@ use warnings;
 
 use LINE::Bot::API::Client;
 use LINE::Bot::API::Response::AudienceGroup;
+use LINE::Bot::API::Response::AudienceMultipleData;
 
 use constant {
     DEFAULT_MESSAGING_API_ENDPOINT => 'https://api.line.me/v2/bot/',
 };
 use Furl;
 use Carp 'croak';
+use URI::Escape;
 
 sub new {
     my($class, %args) = @_;
@@ -45,6 +47,21 @@ sub create_audience_for_uploading {
     LINE::Bot::API::Response::AudienceGroup->new(%{ $res });
 }
 
+sub get_data_for_multiple_audience {
+    my ($self, $opts) = @_;
+
+    my $param = [];
+    push @$param, 'page=' . ($opts->{page} // 1);
+    push @$param, 'description=' . uri_escape_utf8($opts->{description} // '');
+    push @$param, 'status=' . uri_escape_utf8($opts->{status} // '');
+    push @$param, 'size=' .  ($opts->{size} // 20);
+    push @$param, 'includesExternalPublicGroups=' . ($opts->{includesExternalPublicGroups} // '');
+    push @$param, 'createRoute=' . ($opts->{createRoute} // '');
+
+    my $res = $self->request(get => 'audienceGroup/list?' . join('&', @$param));
+    LINE::Bot::API::Response::AudienceMultipleData->new(%{ $res });
+}
+
 1;
 __END__
 
@@ -56,5 +73,9 @@ LINE::Bot::Audience
 Creates an audience for uploading user IDs.
 
 See also the API reference of this method: L<https://developers.line.biz/en/reference/messaging-api/#create-upload-audience-group>
+
+=head1 C<< get_data_for_multiple_audience({ page => "...", description => "...", status => "IN_PROGRESS|READY|FAILED|EXPIRED", size => "...",  includesExternalPublicGroups => "true|false", createRoute => "OA_MANAGER|MESSAGING_API" }) >>
+Gets data for more than one audience.
+See also the API reference of this method: L<https://developers.line.biz/en/reference/messaging-api/#get-audience-groups>
 
 =cut
