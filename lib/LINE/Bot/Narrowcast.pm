bb186a7ef09193a5b7ad23d89eb2df1390975d8a
bedoshi <bedoshidiary.ester.freesia@gmail.com>
2020-05-06 16:24:45 +0900

    add base of narrowcast.pm

---
+++ lib/LINE/Bot/Narrowcast.pm
@@ -0,0 +1,24 @@
+package LINE::Bot::Narrowcast;
+use strict;
+use warnings;
+
+use LINE::Bot::API::Client;
+
+use constant {
+    DEFAULT_MESSAGING_API_ENDPOINT => 'https://api.line.me/v2/bot/',
+};
+
+sub new {
+    my($class, %args) = @_;
+
+    my $client = LINE::Bot::API::Client->new(%args);
+    bless {
+        client               => $client,
+        channel_secret       => $args{channel_secret},
+        channel_access_token => $args{channel_access_token},
+        messaging_api_endpoint => $args{messaging_api_endpoint} // DEFAULT_MESSAGING_API_ENDPOINT,
+    }, $class;
+}
+
+
+1;
