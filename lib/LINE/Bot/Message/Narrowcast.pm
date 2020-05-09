de0f5f11ed911a9e2665ecb53a0a81e495d6b67c
bedoshi <bedoshidiary.ester.freesia@gmail.com>
2020-05-10 03:05:22 +0900

    add send_message.

--- lib/LINE/Bot/Message/Narrowcast.pm
+++ lib/LINE/Bot/Message/Narrowcast.pm
@@ -20,5 +20,31 @@ sub new {
     }, $class;
 }
 
+sub request {
+    my ($self, $method, $path, @payload) = @_;
+
+    return $self->{client}->$method(
+        $self->{messaging_api_endpoint} .  $path,
+        @payload,
+    );
+}
+
+sub send_message {
+    my ($self, $messages, $recipient, $demographic, $limit) = @_;
+
+    my $res = $self->request(
+        post => 'message/narrowcast',
+        +{
+            messages => $messages,
+            recipient => $recipient,
+            filter => {
+                demographic => $demographic,
+            },
+            limit => $limit,
+        },
+    );
+
+    LINE::Bot::API::Response::Common->new(%{ $res });
+}
 
 1;
