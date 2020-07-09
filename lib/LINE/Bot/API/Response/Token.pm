3b983ac3a05206c2381e9490813d539861c763c9
bedoshi <bedoshidiary.ester.freesia@gmail.com>
2020-07-10 01:03:34 +0900

    added member to response object.

--- lib/LINE/Bot/API/Response/Token.pm
+++ lib/LINE/Bot/API/Response/Token.pm
@@ -8,4 +8,7 @@ sub access_token { $_[0]->{access_token} }
 sub expires_in { $_[0]->{expires_in} }
 sub token_type { $_[0]->{token_type} }
 
+sub expires_in { $_[0]->{expires_in} }
+sub key_id { $_[0]->{key_id} }
+
 1;
