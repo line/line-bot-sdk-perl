01551acbbf6faea6a0649a388864a0d7d6e3a12f
Kang-min Liu <gugod@gugod.org>
2021-01-26 17:46:20 +0900

    brief documentation of BotInfo response object

--- lib/LINE/Bot/API/Response/BotInfo.pm
+++ lib/LINE/Bot/API/Response/BotInfo.pm
@@ -12,3 +12,35 @@ sub chatMode       { $_[0]->{chatMode} }
 sub markAsReadMode { $_[0]->{markAsReadMode} }
 
 1;
+
+__END__
+
+=head1 NAME
+
+LINE::Bot::API::Response::BotInfo
+
+=head1 DESCRIPTION
+
+This cllass corresponds to the response object of "Bot Info" API as described in this page: L<https://developers.line.biz/en/reference/messaging-api/#get-bot-info>
+
+For each top-level properties, there is a corresponding method with the same name which provides access to the value of the property.
+
+=head1 METHODS
+
+=over 4
+
+=item userId
+
+=item basicId
+
+=item premiumId
+
+=item displayName
+
+=item pictureUrl
+
+=item chatMode
+
+=item markAsReadMode
+
+=back
